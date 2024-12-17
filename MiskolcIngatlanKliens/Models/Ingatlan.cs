3b578caf64014595b17e46dd3434455442e684c2
TAdrianD <tadriand12@gmail.com>
2024-12-17 13:08:02 +0100

    Osztályok elkészítése

--- MiskolcIngatlanKliens/Models/Ingatlan.cs
+++ MiskolcIngatlanKliens/Models/Ingatlan.cs
@@ -8,5 +8,13 @@ namespace MiskolcIngatlanKliens.Models
 {
     internal class Ingatlan
     {
+        public int Id { get; set; }
+        public string Telepules { get; set; } = null!;
+        public string Cim { get; set; } = null!;
+        public int Ar { get; set; }
+        public string Tipus { get; set; } = null!;
+        public string KepNev { get; set; } = null!;
+        public int UgyintezoId { get; set; }
+        public virtual Ugyintezo? Ugyintezo { get; set; } = null!;
     }
 }
--- MiskolcIngatlanKliens/Models/Ugyintezo.cs
+++ MiskolcIngatlanKliens/Models/Ugyintezo.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MiskolcIngatlanKliens.Models
 {
     internal class Ugyintezo
     {
+        public int Id { get; set; }
+        public string Nev { get; set; } = null!;
+        public string Telefon { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        [JsonIgnore]
+        public virtual ICollection<Ingatlan> Ingatlans { get; set; } = new List<Ingatlan>();
     }
 }
